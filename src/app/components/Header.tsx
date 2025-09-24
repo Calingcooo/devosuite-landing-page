a9cb0c55df17cd67f266eeb79fd95f625264cfbf
Calingcooo <calingco.je@gmail.com>
2025-09-24 15:20:19 +0800

    create header component, create layout wrapper component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { LuMoveLeft } from "react-icons/lu";
+
+const Header = () => {
+  const router = useRouter();
+  return (
+    <header className="border-b border-gray-200 bg-white/80 backdrop-blur-sm">
+      <div className="container mx-auto px-6 py-3">
+        <div className="flex items-center justify-between">
+          <Image
+            src="/assets/banner-logo.png"
+            alt="DevoSuite - Enterprise Solutions Made Simple"
+            className="j-20 w-auto cursor-pointer"
+            onClick={() => router.push("/")}
+            width={500}
+            height={500}
+          />
+          <button
+            className="flex items-center gap-2 text-gray-400 hover:text-gray-500 cursor-pointer"
+            onClick={() => router.push("/")}
+          >
+            <LuMoveLeft className="w-6 h-6" />
+            Back to Home
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css"
+import LayoutWrapper from "./components/LayoutWrapper";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,18 +15,25 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "DevoSuite - Enterprise Solutions Made Simple",
-  description: "Streamline your business operations with DevoSuite's powerful, subscription-based ERP solution. Built for modern enterprises demanding efficiency, scalability, and reliability.",
+  description:
+    "Streamline your business operations with DevoSuite's powerful, subscription-based ERP solution. Built for modern enterprises demanding efficiency, scalability, and reliability.",
   authors: [{ name: "Devolution PH" }],
-  keywords: ["ERP, Enterprise Resource Planning, Business Management, Subscription Software, DevoSuite"],
+  keywords: [
+    "ERP, Enterprise Resource Planning, Business Management, Subscription Software, DevoSuite",
+  ],
   openGraph: {
     title: "DevoSuite - Enterprise Solutions Made Simple",
-    description: "Transform your business operations with our comprehensive ERP solution. Join thousands of companies streamlining their processes with DevoSuite.",
+    description:
+      "Transform your business operations with our comprehensive ERP solution. Join thousands of companies streamlining their processes with DevoSuite.",
     type: "website",
-    images: ["https://ik.imagekit.io/vah26qifw/Devolution%20PH/devosuite/assets/banner_logo_2_a_SPq14g0.png?updatedAt=1758694488288"]
+    images: [
+      "https://ik.imagekit.io/vah26qifw/Devolution%20PH/devosuite/assets/banner_logo_2_a_SPq14g0.png?updatedAt=1758694488288",
+    ],
   },
 };
 
-{/* <meta property="og:title" content="DevoSuite - Enterprise Solutions Made Simple" />l
+{
+  /* <meta property="og:title" content="DevoSuite - Enterprise Solutions Made Simple" />l
 <meta property="og:description" content="Transform your business operations with our comprehensive ERP solution. Join thousands of companies streamlining their processes with DevoSuite." />
 <meta property="og:type" content="website" />
 <meta property="og:image" content="https://lovable.dev/opengraph-image-p98pqg.png" />
@@ -34,7 +42,8 @@ export const metadata: Metadata = {
 <meta name="twitter:site" content="@devosuite" />
 <meta name="twitter:title" content="DevoSuite - Enterprise Solutions Made Simple" />
 <meta name="twitter:description" content="Powerful ERP solution for modern enterprises. Start your free trial today." />
-<meta name="twitter:image" content="https://lovable.dev/opengraph-image-p98pqg.png" /> */}
+<meta name="twitter:image" content="https://lovable.dev/opengraph-image-p98pqg.png" /> */
+}
 
 export default function RootLayout({
   children,
@@ -46,7 +55,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <LayoutWrapper>{children}</LayoutWrapper>
       </body>
     </html>
   );
